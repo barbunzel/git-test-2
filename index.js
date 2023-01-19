e968aaa71b686a03495cbfab2f5e2d9d4609126e
Diego Hernandez <diego.hernandez@e-farm.com>
2023-01-20 00:47:01 +0100

    whatever

--- index.js
+++ index.js
@@ -2,30 +2,3 @@ console.log('hello world');
 
 console.info(1 + 1);
 console.log(2 + 2);
-
-
-
-
-// this is another comment
-
-
-
-// this won't be a conflict
-// this is line 11
-
-
-
-
-
-
-
-// ljsd;lfkjsal;kdfj
-
-
-
-
-
-
-
-
-// hello
